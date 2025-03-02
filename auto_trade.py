48197d93158d10e7a054aad667a74567fe29c933
BlockByBlockAI <blockbyblock.stakespace@gmail.com>
2025-03-01 21:05:48 -0500

    Create auto_trade.py

---
+++ auto_trade.py
@@ -0,0 +1,6 @@
+import logging
+import os
+from datetime import datetime, time
+if __name__ == "__main__":
+    # Default to simulation mode for testing
+    run_automated_trading(simulation_mode=True)
