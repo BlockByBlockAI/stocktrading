7e803dda23b52994cf84d43347f6d938443405f4
BlockByBlockAI <blockbyblock.stakespace@gmail.com>
2025-03-01 21:48:36 -0500

    Delete auto_trade.py

--- auto_trade.py
+++
@@ -1,6 +0,0 @@
-import logging
-import os
-from datetime import datetime, time
-if __name__ == "__main__":
-    # Default to simulation mode for testing
-    run_automated_trading(simulation_mode=True)
